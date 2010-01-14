584055576fdfa4103c4c115666ecb838f8b042bd
adiener <adiener@d291a34c-4c4d-0410-8556-a73de5c6a500>
2010-01-14 01:46:20 +0000

    Create gamepad trunk from template

---
+++ StemLibProjects/gamepad/trunk/source/HelloWorld.h
@@ -0,0 +1,6 @@
+#ifndef __HELLO_WORLD_H__
+#define __HELLO_WORLD_H__
+
+char * helloWorld();
+
+#endif
---
+++ StemLibProjects/gamepad/trunk/test_source/testharness/TestHarness_main.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "HelloWorld.h"
+
+int main() {
+	printf("%s\n", helloWorld());
+	return EXIT_SUCCESS;
+}
